1c9e9f9d49d7a437ef35a08915604f6035b04c60
Thomas Wood <thomas@fastdatascience.com>
2023-08-19 22:12:09 +0100

    shell scripts to validate model

---
+++ create_shell_scripts.sh
@@ -0,0 +1,2 @@
+grep -h python\ generate *  README.md > run_all_models.sh
+grep -h python\ evaluate *  README.md > evaluate_all_models.sh
---
+++ evaluate_all_models.sh
@@ -0,0 +1,18 @@
+Usage: python evaluate_bot_responses_with_mark_scheme.py gpt-3.5-turbo|gpt-4|insolvency_bot_with_gpt-3.5-turbo|insolvency_bot_with_gpt-4 train|test
+COMMAND_LINE_PARAM = f"Usage: python evaluate_bot_responses_with_mark_scheme.py {SUPPORTED_MODELS_CONCAT} train|test"
+python evaluate_responses_with_mark_scheme.py gpt-3.5-turbo train
+python evaluate_responses_with_mark_scheme.py gpt-4 train
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-3.5-turbo train
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-4 train
+python evaluate_responses_with_mark_scheme.py gpt-3.5-turbo test
+python evaluate_responses_with_mark_scheme.py gpt-4 test
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-3.5-turbo test
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-4 test
+python evaluate_responses_with_mark_scheme.py gpt-3.5-turbo train
+python evaluate_responses_with_mark_scheme.py gpt-4 train
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-3.5-turbo train
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-4 train
+python evaluate_responses_with_mark_scheme.py gpt-3.5-turbo test
+python evaluate_responses_with_mark_scheme.py gpt-4 test
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-3.5-turbo test
+python evaluate_responses_with_mark_scheme.py insolvency_bot_with_gpt-4 test
